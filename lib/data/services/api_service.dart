22b432c16c0f33d1a248edf12bf2acd410baf9c2
Sasith Methmal <sasithmjayaweera@gmail.com>
2026-07-20 22:15:52 +0530

    final version 1.0

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -7,10 +7,10 @@ import '../models/dto/plant_code_dto.dart';
 import '../models/dto/schedule_dto.dart';
 
 class ApiService {
-  static const String _baseUrl =
-      'http://bdxdisplayapp.somee.com/api/app_data.asmx';
   // static const String _baseUrl =
-  //     'http://10.76.152.20/display/api/app_data.asmx';
+  //     'http://bdxdisplayapp.somee.com/api/app_data.asmx';
+  static const String _baseUrl =
+      'http://10.76.152.20/display/api/app_data.asmx';
   static const String _apiKey = 'yFlMjSup.IbHOCjyRiTb8QOO9Ltsbr';
   static const String _skey =
       '9c4572c4e6ce5ac08292f1b8affad147794d8a9ad55b2b3f08ae2fa15868ec5f';
